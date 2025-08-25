24dc79cfdd5710ff87c21e77113d375ad1e0148d
Nguyễn Hoàng Phát <143574840+rakannguyen@users.noreply.github.com>
2025-08-25 18:09:29 +0700

    Add files via upload

---
+++ Project_BDS/resources/views/_layout/_layadmin/header.blade.php
@@ -0,0 +1,22 @@
+<header>
+    <nav class="navbar">
+      <div class="navbar__left">
+        <img src="#" alt="Logo" class="navbar__logo">
+      </div>
+
+      <ul class="navbar__menu">
+        <li><a href="/">Trang chủ</a></li>
+      </ul>
+
+      <div class="navbar__right" id="adminMenu" style="display: none;">
+        <div class="dropdown">
+          <button class="dropdown-toggle">Admin ▼</button>
+          <div class="dropdown-menu">
+            <a href="">Đổi ngôn ngữ</a>
+            <a href="#" onclick="logout()">Đăng xuất</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+</header>
+
